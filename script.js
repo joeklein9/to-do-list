e6fd420b3ab146175e72ae67db1f0177854a1a1c
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-01-25 12:15:40 -0500

    finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addTaskBtn = document.getElementById("add-task-btn")
+const clearTasksBtn = document.getElementById("clear-tasks-btn")
 const taskList = document.getElementById ("tasks")
 const taskInput = document.getElementById("task-input")
 
@@ -19,5 +20,11 @@ addTaskBtn.addEventListener("click", function () {
     }
     taskInput.value = ""
 }})
+
+
+clearTasksBtn.addEventListener("click", function (){
+    taskList.innerHTML = ""
+    taskArray = []
+})
 
 
