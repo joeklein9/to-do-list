5737a9687516bb24a3af70ade64918e3236ff618
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-01-25 11:34:01 -0500

    rendering tasks works

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const addTaskBtn = document.getElementById("add-task-btn")
 const taskList = document.getElementById ("tasks")
 const taskInput = document.getElementById("task-input")
 
+const taskArray = []
+
 addTaskBtn.addEventListener("click", function () {
-    taskList.innerHTML += taskInput.value
+    taskArray.push (taskInput.value)
+    for (let i=0; i<taskArray.length; i++) {
+        if (i === taskArray.length - 1) {
+            
+    taskList.innerHTML += `
+    <input type="checkbox" id="task-item-${i}">
+<label for="taskitem-${i}"> ${taskArray[taskArray.length-1]}</label><br> 
+    
+`
+        }
+    }
 })
 
 
